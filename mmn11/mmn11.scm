563b92910f9590dc51596287cbd800e6289f8468
idosh <idoshziv@gmail.com>
2023-03-11 13:44:56 +0200

    refactor

--- mmn11/mmn11.scm
+++ mmn11/mmn11.scm
@@ -17,13 +17,14 @@
     l))
 ; q3
 ; set diff
-(define (set-diff-helper l1 l2 res)
+(define (set-dif-helper l1 l2 res)
   (if (null? l1) res ; if first is empty
     (
-      if (member (car l1) l2) (set-diff-helper (cdr l1) l2 res) ; if first element of l1 is in l2, discard it
-      (set-diff-helper (cdr l1) l2 (cons (car l1) res)) ; if not add it to res
+      if (member (car l1) l2) (set-dif-helper (cdr l1) l2 res) ; if first element of l1 is in l2, discard it
+      (set-dif-helper (cdr l1) l2 (cons (car l1) res)) ; if not add it to res
     ))
   )
-(define (set-diff l1 l2)
-  (let ((in-l1-not-in-l2 (set-diff-helper l1 l2 '())))
-    (set-diff-helper l2 l1 in-l1-not-in-l2)))
+(define (set-dif l1 l2)
+  (let ((in-l1-not-in-l2 (set-dif-helper l1 l2 '())))
+    (set-dif-helper l2 l1 in-l1-not-in-l2)))
+; q4
