e2dceafaf34ba87e4d64190b393a67f410aa9a4b
idosh <idoshziv@gmail.com>
2023-03-11 15:39:35 +0200

    adding q4

--- mmn11/mmn11.scm
+++ mmn11/mmn11.scm
@@ -1,3 +1,4 @@
+#lang racket
 ; q1
 ; custom append
 (define (my_append l m)
@@ -28,3 +29,16 @@
   (let ((in-l1-not-in-l2 (set-dif-helper l1 l2 '())))
     (set-dif-helper l2 l1 in-l1-not-in-l2)))
 ; q4
+(define foo ; define fucntion and name it foo
+ (lambda (ls s) ; create headless function
+    (cond ; cond block
+        [(null? ls) `(() . ,s)] ; if list is empty
+        [(pair? (car ls)) ; if first element of list is a pair
+           (let ((p (foo (car ls) s))) ; runs the recursion on the first item in pair and names it 'p'
+              (let ((p1 (foo (cdr ls) (cdr p)))) ; runs the recursion on the second item in pair and names it 'p1'
+                `(,(cons (car p) (car p1)) . ,(cdr p1))))] ; '`' is for creating a list/ THINKING to add the first item in both
+        [(or (null? (car ls)) (odd? (car ls))) ; if the list is empty or the first item is an odd number
+           (let ((p (foo (cdr ls) s))) ; runs the recursion on the remaining of the list 
+              `(,(cons (car ls) (car p)) . ,(cdr p)))] ; THINKING add the current odd number to the list
+        [else (let ((p (foo (cdr ls) s))) ; an event number, run recursion and name p
+                `(,(car p) . ,(add1 (cdr p))))]))) ; if it is an even number add 1 to the counter - s
